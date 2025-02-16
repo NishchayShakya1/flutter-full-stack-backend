43f8c10f1202bc33b845e1a96aaa2b74b35db8cf
Nishchay Shakya <shakyanischay420@gmail.com>
2025-02-16 21:04:52 +0530

    getAllTweetRepo

--- src/controllers/tweet.controller.ts
+++ src/controllers/tweet.controller.ts
@@ -4,6 +4,7 @@ import {
   createTweetRepo,
   deleteTweetRepo,
   updateTweetRepo,
+  getAllTweetsRepo,
 } from "../repositories/tweet.repository";
 import { ITweetInterface } from "../database/interfaces/tweet.interface";
 import { updateUserWithTweetIdRepo } from "../repositories/user.repository";
@@ -18,6 +19,22 @@ export const getTweetController = async (req: Request, res: Response) => {
       res.status(200).json({ data: tweet });
     } else {
       res.status(500).json({ error: "Tweet not Found" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error });
+  }
+};
+
+export const getAllTweetController = async (req: Request, res: Response) => {
+  // const tweetId = req.params.tweetId as string;
+  // console.log(tweetId);
+  try {
+    const tweets = await getAllTweetsRepo();
+    if (tweets) {
+      res.status(200).json({ data: tweets });
+    } else {
+      res.status(500).json({ error: "Tweets not Found" });
     }
   } catch (error) {
     console.log(error);
--- src/routes/tweet.routes.ts
+++ src/routes/tweet.routes.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { getTweetController, createTweetController,  deleteTweetController, updateTweetController} from "../controllers/tweet.controller";
+import { getTweetController, createTweetController,  deleteTweetController, updateTweetController, getAllTweetController} from "../controllers/tweet.controller";
 
 const tweetRouter = Router();
 
 // Define route paths
 
 tweetRouter.get("/:tweetId", getTweetController);
-// tweetRouter.get("/", getAllTweetController);
+tweetRouter.get("/", getAllTweetController);
 tweetRouter.post("/", createTweetController);
 tweetRouter.delete("/:tweetId", deleteTweetController);
 tweetRouter.put("/", updateTweetController);
